fd9b39e786351b620aa33bc6a05a83eb388c9f28
YuriCGFreire <yurifreireg@gmail.com>
2022-06-08 23:25:10 -0300

    Create user folder, controller, module and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: 'postgres',
+      password: '123456789',
+      database: 'next-db',
+      entities: [
+        join(__dirname, '**', '*.entity.{ts,js}')
+      ],
+      synchronize: true,
+    }),
+    UserModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
