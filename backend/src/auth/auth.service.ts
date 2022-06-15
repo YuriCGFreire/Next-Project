bfcc32260b9d4b0095348e7006492068e8bad9ac
YuriCGFreire <yurifreireg@gmail.com>
2022-06-15 20:56:10 -0300

    Created method validateUSer

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { sign } from 'jsonwebtoken';
 import { Users } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
+import { JwtPayload } from './models/jwt-payload.model';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +12,20 @@ export class AuthService {
         private readonly usersRepository:Repository<Users>
     ){}
 
-    public async createAccessToken(userId: string):Promise<String>{
+    async createAccessToken(userId: string):Promise<String>{
         return sign({userId}, 'segredo', {
             expiresIn: '72h'
         })
+    }
+
+    async validateUser(jwtPayload: JwtPayload){
+        const user = await this.usersRepository.findOne({
+            where: {id: jwtPayload.userId}
+        })
+        return {
+            id: user.id,
+            name: user.name,
+            admin: user.admin
+        }
     }
 }
