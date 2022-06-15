e615514c36802cfe4cf1cb8aee7493a4d806bf1c
YuriCGFreire <yurifreireg@gmail.com>
2022-06-15 20:43:38 -0300

    Created method createAccessToken

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { sign } from 'jsonwebtoken';
+import { Users } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(Users)
+        private readonly usersRepository:Repository<Users>
+    ){}
+
+    public async createAccessToken(userId: string):Promise<String>{
+        return sign({userId}, 'segredo', {
+            expiresIn: '72h'
+        })
+    }
+}
