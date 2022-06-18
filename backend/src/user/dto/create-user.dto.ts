477cbcd727b9a2df82d5b92a3b0890ca6037305d
YuriCGFreire <yurifreireg@gmail.com>
2022-06-18 19:34:07 -0300

    Added the class-validator

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { IsBoolean, IsNotEmpty, Matches, MinLength} from "class-validator";
 
 export class CreateUserDto {
+
+    @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
     age: number;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
     password: string;
+
+    @IsBoolean()
     admin?: boolean;
 }
