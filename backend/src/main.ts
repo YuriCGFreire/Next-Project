ff70a79056de65398f65e4e7fc4d47311425efd0
YuriCGFreire <yurifreireg@gmail.com>
2022-06-28 17:03:02 -0300

    Changed project port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document)
   app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}))
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
